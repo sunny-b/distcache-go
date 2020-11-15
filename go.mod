0ff9b4aa67184dfaaa44b72c778a381caa690129
Sunny <sunny@Iriss-MacBook-Pro-2.local>
2020-11-15 17:10:15 -0500

    add working insert method

--- go.mod
+++ go.mod
@@ -1,5 +1,9 @@
-module github.com/sunny-b/linkedlist
+module github.com/sunny-b/distcache-go
 
 go 1.15
 
-require golang.org/x/tools/gopls v0.5.3 // indirect
+require (
+	golang.org/x/lint v0.0.0-20200302205851-738671d3881b // indirect
+	golang.org/x/tools v0.0.0-20201114224030-61ea331ec02b // indirect
+	golang.org/x/tools/gopls v0.5.3 // indirect
+)
--- list/list.go
+++ list/list.go
@@ -1,5 +1,7 @@
 package list
 
+import "errors"
+
 /*
  Doubly LinkedList
 
@@ -12,6 +14,7 @@ package list
  - Insert(ele, pos)
  - Remove(pos)
  - Replace(value, pos)
+ - GetRootVal
 
  Node: object
  - next
@@ -22,6 +25,11 @@ package list
  - len
  - root (head, with access to tail)
 */
+
+var (
+	// ErrInvalidIndex occurs when the specified index is out of range
+	ErrInvalidIndex = errors.New("invalid positional index")
+)
 
 // List is the LinkedList struct
 type List struct {
@@ -39,17 +47,66 @@ type Node struct {
 // New creates a new LinkedList
 func New() *List {
 	return &List{
-		root: new(Node),
-		len:  0,
+		root: &Node{
+			next: new(Node),
+			prev: new(Node),
+		},
+		len: 0,
 	}
 }
 
+// Length returns the length of the LinkedList
+func (l *List) Length() int {
+	return l.len
+}
+
 // InsertRoot inserts a value into the LinkedList
-func (l *List) InsertRoot(val interface{}) bool {
+func (l *List) InsertRoot(val interface{}) *List {
 	if l.len == 0 {
 		l.root.value = val
 		l.len++
 	}
 
-	return true
+	return l
+}
+
+// GetRootVal returns the value at the root node
+func (l *List) GetRootVal() interface{} {
+	return l.root.value
+}
+
+// Insert inserts a value at the positional index
+func (l *List) Insert(val interface{}, pos int) (*List, error) {
+	if pos < 0 || pos > l.len {
+		return l, ErrInvalidIndex
+	}
+
+	currentIdx := 0
+	currentNode := l.root
+
+	// start at 0
+	// iterate over nodes until we get to position
+	for currentIdx != pos-1 {
+		currentNode = currentNode.next
+		currentIdx++
+	}
+
+	// save the prev of the current node as temp var
+	temp := currentNode.prev
+
+	// create new node with val
+	// make the next of the new node be the current node
+	newNode := &Node{
+		value: val,
+	}
+	newNode.next = currentNode
+
+	// put prev of new node as the temp var
+	newNode.prev = temp
+	// put the next of the temp var as the new node
+	temp.next = newNode
+
+	l.len++
+
+	return l, nil
 }
